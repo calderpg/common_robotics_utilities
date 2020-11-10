fcea49e01d4f75a0b0b74fad7a6c1f9e8c0cf7e7
Michel Hidalgo <michel@ekumenlabs.com>
2020-11-10 17:14:20 -0300

    Construct rclcpp::Time explictly.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- test/ros_helpers_test.cpp
+++ test/ros_helpers_test.cpp
@@ -9,21 +9,24 @@ namespace ros_helpers
 namespace
 {
 
-#if COMMON_ROBOTICS_UTILITIES__SUPPORTED_ROS_VERSION == 2
-using RosDuration = rclcpp::Duration;
-#elif COMMON_ROBOTICS_UTILITIES__SUPPORTED_ROS_VERSION == 1
-using RosDuration = ros::Duration;
-#endif
-
 GTEST_TEST(ROSHelpersTest, SetMessageTimestamps)
 {
   VisualizationMarkerArray marker_array;
   marker_array.markers.resize(5);
   // Note that the ROS time type depends on which ROS variant is in use.
-  // Get marker base timestamp (ensuring clock sources match).
-  const RosTime base_stamp = marker_array.markers[0].header.stamp;
-  // Advance timestamps by one second.
-  const RosTime expected_stamp = base_stamp + RosDuration(1, 0);
+#if COMMON_ROBOTICS_UTILITIES__SUPPORTED_ROS_VERSION == 2
+  // Base time measured against ROS clock.
+  const rclcpp::Time base_stamp(0LL, RCL_ROS_TIME);
+  // Advance time by one second.
+  const rclcpp::Time expected_stamp =
+    base_stamp + rclcpp::Duration(1, 0);
+#elif COMMON_ROBOTICS_UTILITIES__SUPPORTED_ROS_VERSION == 1
+  // Default constructed base time.
+  const ros::Time base_stamp;
+  // Advance time by one second.
+  const ros::Time expected_stamp =
+    base_stamp + ros::Duration(1.0);
+#endif
   SetMessageTimestamps(marker_array, expected_stamp);
   for (const auto& marker : marker_array.markers) {
     EXPECT_EQ(marker.header.stamp, expected_stamp);

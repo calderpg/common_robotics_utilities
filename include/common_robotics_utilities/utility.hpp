4b6a714e2d2c3f8a5dfb193b541b25dc343cbf65
Calder Phillips-Grafflin <calder.pg@gmail.com>
2022-08-10 14:56:41 -0400

    Remove nearest neighbor early return from RRT planners, remove branch prediction hints

--- include/common_robotics_utilities/utility.hpp
+++ include/common_robotics_utilities/utility.hpp
@@ -12,27 +12,6 @@
 
 #include <Eigen/Geometry>
 
-// Branch prediction hints
-// Figure out which compiler we have
-#if defined(__clang__)
-  /* Clang/LLVM */
-  #define CRU_LIKELY(x) __builtin_expect(!!(x), 1)
-  #define CRU_UNLIKELY(x) __builtin_expect(!!(x), 0)
-#elif defined(__ICC) || defined(__INTEL_COMPILER)
-  /* Intel ICC/ICPC */
-  #define CRU_LIKELY(x) __builtin_expect(!!(x), 1)
-  #define CRU_UNLIKELY(x) __builtin_expect(!!(x), 0)
-#elif defined(__GNUC__) || defined(__GNUG__)
-  /* GNU GCC/G++ */
-  #define CRU_LIKELY(x) __builtin_expect(!!(x), 1)
-  #define CRU_UNLIKELY(x) __builtin_expect(!!(x), 0)
-#elif defined(_MSC_VER)
-  /* Microsoft Visual Studio */
-  /* MSVC doesn't support branch prediction hints. Use PGO instead. */
-  #define CRU_LIKELY(x) (x)
-  #define CRU_UNLIKELY(x) (x)
-#endif
-
 // Macro to disable unused parameter compiler warnings
 #define CRU_UNUSED(x) (void)(x)
 
